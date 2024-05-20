5683c6a7bd2b09a5252615f136965a46f22492f6
Miezhiko <Miezhiko@gmail.com>
2024-05-20 16:44:38 +0400

    move some types to Types, rename types
    Signed-off-by: Miezhiko <Miezhiko@gmail.com>

--- src/Futures.hs
+++ src/Futures.hs
@@ -9,44 +9,21 @@ module Futures
 import           Prelude.Unicode
 
 import           Config
+import           Types
 
 import           Wuss
 
 import           Control.Concurrent (forkIO)
 import           Control.Monad      (forever, unless, void)
-import           Data.Aeson
+import           Data.Aeson         (decode)
 import qualified Data.Text          as T
 import qualified Data.Text.IO       as TIO
 import           Network.WebSockets (ClientApp, receiveData, sendClose, sendTextData)
 
 import           NeatInterpolation  (text)
 
-data JsonData
-  = JsonData
-      { topic :: T.Text
-      , data' :: Data
-      }
-  deriving (Show)
-
-data Data
-  = Data
-      { s :: T.Text
-      , b :: [[T.Text]]
-      }
-  deriving (Show)
-
-instance FromJSON JsonData where
-  parseJSON (Object v) =
-    JsonData <$> v.: "topic" <*> v.: "data"
-  parseJSON _ = fail "Expected an object"
-
-instance FromJSON Data where
-  parseJSON (Object v) =
-    Data <$> v.: "s" <*> v.: "b"
-  parseJSON _ = fail "Expected an object"
-
-extractValues ∷ JsonData -> Maybe (T.Text, T.Text)
-extractValues (JsonData _ myData) =
+extractValues ∷ OrderData -> Maybe (T.Text, T.Text)
+extractValues (OrderData _ myData) =
   let bList = b myData
   in if null bList
      then Nothing
@@ -58,7 +35,7 @@ ws connection = do
 
   void ∘ forkIO ∘ forever $ do
     message <- receiveData connection
-    let jsonData = decode message :: Maybe JsonData
+    let jsonData = decode message :: Maybe OrderData
     case jsonData of
       Just dat ->
         case (extractValues dat) of
@@ -90,5 +67,5 @@ ws connection = do
 
 go ∷ Conf -> IO ()
 go _cfg = do
-  runSecureClient "stream.bybit.com" 443 "/v5/public/linear" ws
-  putStrLn $ "a"
+  runSecureClient "stream.bybit.com"
+                  443 "/v5/public/linear" ws
--- src/Types.hs
+++ src/Types.hs
@@ -1,3 +1,31 @@
 module Types
-  (
+  ( OrderData (..)
+  , OrderDetails (..)
   ) where
+
+import           Data.Aeson
+import qualified Data.Text  as T
+
+data OrderData
+  = OrderData
+      { topic :: T.Text
+      , data' :: OrderDetails
+      }
+  deriving (Show)
+
+data OrderDetails
+  = OrderDetails
+      { s :: T.Text
+      , b :: [[T.Text]]
+      }
+  deriving (Show)
+
+instance FromJSON OrderData where
+  parseJSON (Object v) =
+    OrderData <$> v.: "topic" <*> v.: "data"
+  parseJSON _ = fail "Expected an object"
+
+instance FromJSON OrderDetails where
+  parseJSON (Object v) =
+    OrderDetails <$> v.: "s" <*> v.: "b"
+  parseJSON _ = fail "Expected an object"
